9c2ccb42403f955927a6ac008263d15832ee4498
ayehu1alem <165663748+ayehu1alem@users.noreply.github.com>
2024-07-15 14:58:07 +0300

    Add files via upload
    Calculate area of a wall

---
+++ WallArea/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Wall wall = new Wall(-1.25,-1.25);
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+    }
+}
